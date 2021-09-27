1f8bb1a66930185ca0f4155f3ab3ee7897b18610
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-27 15:58:39 -0300

    Criar classes Imovel, Moradores e Imobiliaria

---
+++ src/br/com/zup/Imobiliaria.java
@@ -0,0 +1,8 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Imobiliaria {
+    private List <Imovel> imovel = new ArrayList<Imovel>();
+}
---
+++ src/br/com/zup/Imovel.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Imovel {
+    private String endereço;
+    private double valorAluguel;
+    private String funcionarioResponsavel;
+    private List<Moradores>moradores = new ArrayList<Moradores>();
+
+}
